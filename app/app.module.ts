d57cdfe0d582c17b35578979bb1fd6f293604a9f
majortomdev <kinahanj25@gmail.com>
2018-11-25 22:07:58 +0000

    added some styling, some mainpage work

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent} from './components/settings.component';
 import { PageComponent } from './components/page.component';
 import { DateblockComponent } from './dateblock/dateblock.component';
+import { SidebarComponent } from './components/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SettingsComponent,
     PageComponent,
-    DateblockComponent
+    DateblockComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
--- app/headings.component.spec.ts
+++ app/headings.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HeadingsComponentComponent } from './headings-component.component';
-
-describe('HeadingsComponentComponent', () => {
-  let component: HeadingsComponentComponent;
-  let fixture: ComponentFixture<HeadingsComponentComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeadingsComponentComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeadingsComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { HeadingsComponentComponent } from './headings-component.component';
+//
+// describe('HeadingsComponentComponent', () => {
+//   let component: HeadingsComponentComponent;
+//   let fixture: ComponentFixture<HeadingsComponentComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HeadingsComponentComponent ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HeadingsComponentComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
